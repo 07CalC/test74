089ee668605f459cd88f49efde23f9e740842081
CalC <96346957+07CalC@users.noreply.github.com>
2025-11-08 22:07:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const port = 6124;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('Hello, World! again mfs\n');
 });
+
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
