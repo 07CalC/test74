1ee976a9d8383a996b53a8541e3cf37688184a67
CalC <96346957+07CalC@users.noreply.github.com>
2025-11-23 15:41:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const hostname = '0.0.0.0';
 const port = 6124;
 
 const server = http.createServer((req, res) => {
+  console.log("new request came");
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World! again mfs once more, here we go again test number 69\n');
